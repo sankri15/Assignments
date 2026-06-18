efcac2a3be16ee52ef0e4d597251631d1f0b21f4
sankri15 <sanjanapal004@gmail.com>
2026-06-18 15:54:03 +0530

    Update script.js

--- Assignments/JS Assignment-2/script.js
+++ Assignments/JS Assignment-2/script.js
@@ -1,38 +1,73 @@
-// 1. Display even numbers from 1 to 100
-function printEvenNumbers() {
-    for (let i = 1; i <= 100; i++) {
-        if (i % 2 === 0) {
-            console.log(i);
-        }
+// helper
+function addQA(question, answer) {
+    document.getElementById("container").innerHTML += `
+        <div class="box">
+            <div class="question">${question}</div>
+            <div class="answer">Ans: ${JSON.stringify(answer, null, 2)}</div>
+        </div>
+    `;
+}
+
+
+// Q1) Even numbers from 1 to 100
+let evenNumbers = [];
+
+for(let i=1;i<=100;i++){
+    if(i % 2 === 0){
+        evenNumbers.push(i);
     }
 }
-printEvenNumbers();
+
+addQA(
+"Q1) Display numbers from 1 to 100 but only even numbers",
+evenNumbers
+);
 
 
-// 2. Calculator using switch
-function calculator(n1, n2, operator) {
-    switch (operator) {
+// Q2) Calculator using switch
+function calculator(a, b, op){
+    let result;
+
+    switch(op){
         case "+":
-            return n1 + n2;
+            result = a + b;
+            break;
+
         case "-":
-            return n1 - n2;
+            result = a - b;
+            break;
+
         case "*":
-            return n1 * n2;
+            result = a * b;
+            break;
+
         case "/":
-            return n2 !== 0 ? n1 / n2 : "Cannot divide by zero";
+            result = a / b;
+            break;
+
         default:
-            return "Invalid operator";
+            result = "Invalid operator";
     }
+
+    return result;
 }
 
-console.log(calculator(10, 5, "+"));
+addQA(
+"Q2) Calculator using switch (add, subtract, multiply, divide)",
+{
+    "10 + 5": calculator(10,5,"+"),
+    "10 - 5": calculator(10,5,"-"),
+    "10 * 5": calculator(10,5,"*"),
+    "10 / 5": calculator(10,5,"/")
+}
+);
 
 
-// 3. Tax calculation using switch
-function findTax(salary) {
-    let tax = 0;
+// Q3) Tax calculation using switch
+function findTax(salary){
+    let tax;
 
-    switch (true) {
+    switch(true){
         case (salary > 0 && salary <= 500000):
             tax = 0;
             break;
@@ -56,24 +91,40 @@ function findTax(salary) {
     return tax;
 }
 
-console.log(findTax(750000));
+addQA(
+"Q3) Find tax using switch case",
+{
+    "Salary 400000": findTax(400000),
+    "Salary 800000": findTax(800000),
+    "Salary 1200000": findTax(1200000),
+    "Salary 2000000": findTax(2000000)
+}
+);
 
 
-// 4. Sum of products of corresponding digits
-function sumOfProducts(n1, n2) {
+// Q4) Sum of products of corresponding digits
+function sumOfProducts(n1, n2){
     let s1 = n1.toString().split("").reverse();
     let s2 = n2.toString().split("").reverse();
 
     let len = Math.max(s1.length, s2.length);
+
     let sum = 0;
 
-    for (let i = 0; i < len; i++) {
-        let d1 = parseInt(s1[i] || 0);
-        let d2 = parseInt(s2[i] || 0);
-        sum += d1 * d2;
+    for(let i=0;i<len;i++){
+        let digit1 = parseInt(s1[i]) || 0;
+        let digit2 = parseInt(s2[i]) || 0;
+
+        sum += digit1 * digit2;
     }
 
     return sum;
 }
 
-console.log(sumOfProducts(6, 34));
+addQA(
+"Q4) Sum of products of corresponding digits",
+{
+    "6 & 34": sumOfProducts(6,34),
+    "123 & 45": sumOfProducts(123,45)
+}
+);
